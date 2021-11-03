06b453ee3715e49d9dc131dddef28196b3a6b2a8
Sai Tejaswee Vissapragada <teja108116@gmail.com>
2021-11-03 18:32:49 -0500

    commit

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # fwb38vissapragada
 Assignment-10
 <a href="https://fwb38vissapragada.herokuapp.com/">Link to Heroku: https://fwb38vissapragada.herokuapp.com/ </a>
+
+Class - icecream class dog{ constructor(breed,price,colour){
+    this.breed = breed;
+    this.price = price;
+    this.colour = colour;s
+}
+}
--- routes/selector.js
+++ routes/selector.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('selector', { title: 'selector' });
 });
-router.get('/', function(req, res, next) {
-  value++;
-  res.send(`user accessess are : ${value}`);
-});
+ 
 module.exports = router;
